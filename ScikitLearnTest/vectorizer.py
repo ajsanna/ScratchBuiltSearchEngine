0dace686758aea75aab20f71f1fec09ebd17ce55
Ryan <98720555+rwlarson2024@users.noreply.github.com>
2024-05-08 15:35:37 -0700

    Vectorizer.py test
    Tests the scikitlearn vectorization and stemming words of nltk on a provided text document

---
+++ ScikitLearnTest/vectorizer.py
@@ -0,0 +1,24 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+#nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+courpus = [
+    'this is document one map',
+    'this is the second document mapping',
+    'and this is the third one maps.',
+    'Is this the first document documents and document',
+]
+stemmer = PorterStemmer()
+custom_stopwords = stopwords.words('english')
+def tokenize_and_stem(text):
+    tokens = nltk.word_tokenize(text)
+    stemmed_tokens = [stemmer.stem(token)for token in tokens if token.lower() not in custom_stopwords]
+    return stemmed_tokens
+
+vectorizer = TfidfVectorizer(tokenizer=tokenize_and_stem)
+x = vectorizer.fit_transform(courpus)
+print(vectorizer.get_stop_words())
+print(vectorizer.get_feature_names_out())
+print(x.shape)
